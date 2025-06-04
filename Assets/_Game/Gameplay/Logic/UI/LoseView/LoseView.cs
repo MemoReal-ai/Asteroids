e7e22817dafdff7330b09cb5774e3095ce58a2ad
Vlad <elemadara@gmail.com>
2025-06-04 15:52:47 +0400

    Add animation to loseView

--- Assets/_Game/Gameplay/Logic/UI/LoseView/LoseView.cs
+++ Assets/_Game/Gameplay/Logic/UI/LoseView/LoseView.cs
@@ -1,31 +1,41 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 namespace _Game.Gameplay.Logic.UI
 {
     public class LoseView : MonoBehaviour
     {
-         public TextMeshProUGUI PointLabel;
-         public Button RestartButton;
-         public Button QuitButton;
+        public TextMeshProUGUI PointLabel;
+        public Button RestartButton;
+        public Button QuitButton;
+
+        [SerializeField] private RectTransform _losePanel;
+        [SerializeField] private float _durationAnimationShow;
+
+        private Vector3 _losePanelDefaultLocalScale;
 
         private void Start()
         {
+            _losePanelDefaultLocalScale = _losePanel.localScale;
             Hide();
         }
 
         public void ShowPoints(string points)
         {
-            PointLabel.text=points;
+            PointLabel.text = points;
         }
+
         public void Show()
         {
             gameObject.SetActive(true);
+            _losePanel.DOScale(_losePanelDefaultLocalScale, _durationAnimationShow).SetEase(Ease.OutBounce);
         }
 
-        public void Hide()
+        private void Hide()
         {
+            _losePanel.localScale = Vector3.zero;
             gameObject.SetActive(false);
         }
     }
